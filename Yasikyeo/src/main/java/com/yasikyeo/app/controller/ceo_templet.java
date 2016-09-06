8e8327b54851a3d5c4b13fb1a7eabcccfdf4a0c4
User <User@UserPC85>
2016-09-06 15:12:01 +0900

    컨트롤러 테스트

---
+++ Yasikyeo/src/main/java/com/yasikyeo/app/controller/ceo_templet.java
@@ -0,0 +1,17 @@
+package com.yasikyeo.app.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ceo_templet {
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	@RequestMapping("ceo_templet/marketmember_reg4.do")
+	public void marketmember_reg4(){
+		logger.info("marketmember_reg4 페이지 로딩");
+	}
+}
