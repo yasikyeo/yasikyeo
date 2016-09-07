59fdc4b4f0d369789bb9bea341ad1ca97ad51738
User <User@UserPC70>
2016-09-07 17:01:16 +0900

    top2.jsp 추가 by 성욱

---
+++ Yasikyeo/src/main/java/com/yasikyeo/app/TempletTestContoller.java
@@ -0,0 +1,12 @@
+package com.yasikyeo.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TempletTestContoller {
+
+	@RequestMapping("/index2.do")
+	public void index2(){
+	}
+}
