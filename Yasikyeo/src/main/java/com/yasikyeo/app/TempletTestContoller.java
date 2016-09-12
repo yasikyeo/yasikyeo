927fbbbc0ee98d0f5eb8e2e9b200598a9bbbc099
User <User@UserPC70>
2016-09-12 14:03:33 +0900

    mypage.jsp 추가(미완성) templeTestcontroller 부활 by 성욱

--- Yasikyeo/src/main/java/com/yasikyeo/app/TempletTestContoller.java
+++ Yasikyeo/src/main/java/com/yasikyeo/app/TempletTestContoller.java
@@ -1,4 +1,4 @@
-/*package com.yasikyeo.app;
+package com.yasikyeo.app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TempletTestContoller {
 
-	@RequestMapping("/index2.do")
-	public void index2(){
-	}
-	
-	@RequestMapping("/login/client_addmember.do")
-	public void addmember(){
-	}
-	@RequestMapping("/login/client_findPwd.do")
-	public void client_findPwd(){
+	@RequestMapping("/csuTest1.do")
+	public String mypage(){
+		return "mypage/client_mypage";
 	}
 }
-*/
