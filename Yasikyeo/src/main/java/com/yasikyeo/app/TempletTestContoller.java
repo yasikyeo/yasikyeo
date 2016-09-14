c465fad0bfd2c5a1ecb258fa7a1fcbc746e813db
User <User@UserPC70>
2016-09-14 23:45:55 +0900

    templecontoller에 완성된 페이지 표시 by 성욱

--- Yasikyeo/src/main/java/com/yasikyeo/app/TempletTestContoller.java
+++ Yasikyeo/src/main/java/com/yasikyeo/app/TempletTestContoller.java
@@ -8,14 +8,17 @@ public class TempletTestContoller {
 
 	@RequestMapping("/csuTest1.do")
 	public String mypage(){
+		//완성
 		return "mypage/client_mypage";
 	}
 	@RequestMapping("/csuTest2.do")
 	public String paymentst(){
+		//완성
 		return "mypage/client_paymentList";
 	}
 	@RequestMapping("/csuTest3.do")
 	public String findpwd(){
+		//완성
 		return "mypage/client_myinfo";
 	}
 }
