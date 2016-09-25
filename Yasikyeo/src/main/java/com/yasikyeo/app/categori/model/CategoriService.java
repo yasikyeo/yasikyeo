17fc4bf57209bf783e43aae6aaf771d5110e3218
User <User@UserPC70>
2016-09-26 06:37:34 +0900

    성욱

---
+++ Yasikyeo/src/main/java/com/yasikyeo/app/categori/model/CategoriService.java
@@ -0,0 +1,5 @@
+package com.yasikyeo.app.categori.model;
+
+public interface CategoriService {
+
+}
---
+++ Yasikyeo/src/main/java/com/yasikyeo/app/categori/model/CategoriVO.java
@@ -0,0 +1,7 @@
+package com.yasikyeo.app.categori.model;
+
+public class CategoriVO {
+	private int categoriNo;
+	private String categoriName;
+	private int shopNo;
+}
